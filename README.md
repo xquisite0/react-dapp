e24db304378467bfd56946fdce9ba0277d9d8562
Zhou Yuhang <zyuhang255@gmail.com>
2022-02-24 16:04:44 +0800

    completed project

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple decentralized application using React, Hardhat, Solidity & Javascript. 
+Simple web UI allows user to connect their wallets and modify state variables on contracts deployed and retrieve data.
+It also allows user to send and receive fake tokens to other users.
---
+++ scripts/deploy.js
@@ -0,0 +1,32 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
+
+  const Greeter = await hre.ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, World!");
+
+  const Token = await hre.ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+
+  const NDToken = await hre.ethers.getContractFactory("NDToken");
+  const ndToken = await NDToken.deploy("Nader Dabit Token", "NDT");
+
+  await greeter.deployed();
+  await token.deployed();
+
+  console.log("Greeter deployed to:", greeter.address);
+  console.log("Token deployed to:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
